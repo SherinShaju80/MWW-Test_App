8df6f02a1b70f7a6e385a97d89697d494870fcf1
Sherin Shaju <sherinshaju80@gmail.com>
2022-06-02 14:18:23 +0530

    changes in package

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,16 @@ import "dotenv/config";
  * @type {import('vite').UserConfig}
  */
 export default {
+  mode: "development",
   define: {
     "process.env.SHOPIFY_API_KEY": JSON.stringify(process.env.SHOPIFY_API_KEY),
   },
   plugins: [react()],
+  clearScreen: false,
   server: {
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
     hmr: {
       port: 443,
     },
